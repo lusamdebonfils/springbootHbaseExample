66c50c8a1317a522da8c5f87aa097ebcb54da09c
lusamdebonfils <samuelluswata@gmail.com>
2020-10-01 23:34:25 -0400

    Added springboot with hbase dependency in the pom file

---
+++ dockerspringhbase/src/main/java/com/example/dockerspringhbase/DockerspringhbaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringhbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerspringhbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerspringhbaseApplication.class, args);
+	}
+
+}
---
+++ dockerspringhbase/src/test/java/com/example/dockerspringhbase/DockerspringhbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringhbase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerspringhbaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
